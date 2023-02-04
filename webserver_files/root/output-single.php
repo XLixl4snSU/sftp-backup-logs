8212efa2edda9327bb29237d8ad2651ad3a55734
XLixl4snSU <111450124+XLixl4snSU@users.noreply.github.com>
2023-02-05 00:20:08 +0100

    Update output-single.php

--- webserver_files/root/output-single.php
+++ webserver_files/root/output-single.php
@@ -9,9 +9,13 @@ $filename = "../logs/backup_script-" . $date . ".log";
 <h2>Logs for backup of <?php echo $date ?></h2>
 <?php
 $fh = fopen($filename, 'r');
-$pageText = fread($fh, 25000);
-$erg = $converter->convert($pageText);
-$erg = str_replace("color: white","color: black",$erg);
-$erg = str_replace("background-color: black;","",$erg);
-echo nl2br($erg);
+if(filesize($filename) > 0) {
+  $pageText = fread($fh, filesize($filename));
+  $erg = $converter->convert($pageText);
+  $erg = str_replace("color: white","color: black",$erg);
+  $erg = str_replace("background-color: black;","",$erg);
+  echo nl2br($erg);
+} else
+  echo "Error. This log does not exist.";
+}
 ?>
